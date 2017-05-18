66c0d3949cc076a8bec3252e9a81c1b5a64d446c
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-05-18 14:15:30 -0400

    basic props passed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import TodoList from 'TodoList';
 
 export default class Main extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Mow the lawn'
+        }
+      ]
+    }
   };
 
   render() {
+    let {todos} = this.state;
+
     return (
       <div>
-        Main.jsx
-      </div>      
+        <TodoList todos={todos}/>
+      </div>
     )
   }
 }
