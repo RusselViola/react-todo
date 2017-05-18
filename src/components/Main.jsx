ffeef4bcab5a0026df78876ed09858be0707adb4
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-05-18 14:02:08 -0400

    Webpack Alias configured

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,15 @@
-import React { Component } from 'react';
+import React, { Component } from 'react';
 
 export default class Main extends Component {
   constructor(props) {
     super(props);
   };
 
-  render(){
-    <div>
-      Main.jsx
-    </div>
+  render() {
+    return (
+      <div>
+        Main.jsx
+      </div>      
+    )
   }
 }
