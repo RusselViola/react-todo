ca7d44d11a894678e4e209913ef53a191172bda6
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-05-19 18:01:20 -0400

    todos render one component for each todo item

--- src/tests/components/TodoList.test.jsx
+++ src/tests/components/TodoList.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoList from 'TodoList';
+import Todo from 'Todo';
 
 const expect = require('expect');
 const $ = require('jQuery');
@@ -9,5 +10,22 @@ const TestUtils = require('react-addons-test-utils');
 describe('TodoList', () => {
   it('should exist', () => {
     expect(TodoList).toExist();
+  });
+
+  it('should render one Todo component for each todo item', () => {
+    let todos = [
+      {
+        id: 1,
+        text: 'Do something'
+      },
+      {
+        id: 2,
+        text: 'Check mail'
+      }
+    ];
+    let todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    let todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
   });
 });
