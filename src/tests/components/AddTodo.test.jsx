d4749517041b33426099c834cc4d9af1223297ce
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-05-24 18:30:45 -0400

    AddTodo spy detecting form input

--- src/tests/components/AddTodo.test.jsx
+++ src/tests/components/AddTodo.test.jsx
@@ -10,4 +10,15 @@ describe('AddTodo', () => {
   it('should exist', () => {
     expect(AddTodo).toExist();
   });
+
+  it('should call onAddTodo props with valid data', () => {
+    let spy = expect.createSpy();
+    let addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    let $el = $(ReactDOM.findDOMNode(addTodo));
+
+    addTodo.refs.todoText.value = 'Check mail';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toHaveBeenCalledWith('Check mail');
+  })
 });
