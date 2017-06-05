11a2b4f40cfd8e81566ce571c2f9333f0f1f2741
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-06-05 15:42:52 -0400

    valid todos array tested

--- src/tests/api/TodoAPI.test.jsx
+++ src/tests/api/TodoAPI.test.jsx
@@ -5,4 +5,23 @@ describe('TodoAPI', () => {
   it('should exist', () => {
     expect(TodoAPI).toExist();
   });
+
+  describe('setTodos', () => {
+    it('should set valid todos array', () => {
+      let todos = [{
+        id: 23,
+        text: 'Test all files',
+        completed: false
+      }];
+      TodoAPI.setTodos(todos);
+
+      let actualTodos = JSON.parse(localStorage.getItem('todos'));
+
+      expect(actualTodos).toEqual(todos);
+    });
+  });
+
+  describe('getTodos', () => {
+
+  });
 });
