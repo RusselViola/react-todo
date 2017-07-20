dc5aef052219d534f02c9d06de0cb6f34d972e4f
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-07-20 17:13:52 -0400

    setSearchText action created and one test working

---
+++ src/tests/actions/actions.test.jsx
@@ -0,0 +1,14 @@
+const expect = require('expect');
+const actions = require('actions');
+
+describe('Actions', () => {
+  it('should generate search text action', () => {
+    let action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText: 'Some search text'
+    };
+    let res = actions.setSearchText(action.searchText);
+
+    expect(res).toEqual(action);
+  });
+});
