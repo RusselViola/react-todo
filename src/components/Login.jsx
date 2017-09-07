742d15c9e8c2d0137c6758dfa443129ab256d96a
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-09-07 17:51:20 -0400

    basic login page with some foundation added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,22 @@ import React, { Component } from 'react';
 export default class Login extends Component {
 
   render() {
-    return <p>Login rendering component</p>
+    return (
+      <div>
+        <h1 className="page-title">Tododux</h1>
+
+        <div className="row">
+          <div className="columns small-centered small-10 medium-6 large-4">
+            <div className="callout callout-auth">
+              <h3>Login</h3>
+              <p>
+                Login with GitHub account below.
+              </p>
+              <button className="button">Login With GitHub</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
